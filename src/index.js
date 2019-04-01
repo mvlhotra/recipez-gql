46123ae17c480949d51cd8a65a77d0d108120d06
Nik Malhotra <nmal@me.com>
2019-03-31 20:12:16 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,18 @@
 const { GraphQLServer } = require('graphql-yoga')
 
+
 const typeDefs = `
 type Query {
   info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
 }
 `
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
 
-// 2
 const resolvers = {
   Query: {
-    creation: () => `Eggs Benny`,
-    feed: () => links,
+    info: () => `This is the API of a Hackernews Clone`
   }
 }
 
-// 3
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
