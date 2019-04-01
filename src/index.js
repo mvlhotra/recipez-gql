0aafbd7c4757baf3658a8e9ddb4a494e19bf3820
Nik Malhotra <nmal@me.com>
2019-04-01 15:23:23 -0400

    generate prisma schema

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const { GraphQLServer } = require('graphql-yoga')
 
+const { prisma } = require('./generated/prisma-client')
+
+async function main() {
+
+  // Create a new link
+  const newLink = await prisma.createLink({
+    url: 'www.prisma.io',
+    description: 'Prisma replaces traditional ORMs',
+  })
+  console.log(`Created new link: ${newLink.url} (ID: ${newLink.id})`)
+
+  // Read all links from the database and print them to the console
+  const allLinks = await prisma.links()
+  console.log(allLinks)
+}
+
+main().catch(e => console.error(e))
 
 
 let links = [{
